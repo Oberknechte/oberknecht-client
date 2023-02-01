4dce5ea0dde7310ab54b6abebd406a8b271da41e
jubewe <ju.be.we@posteo.de>
2023-02-01 20:54:58 +0100

    vb-0.0.2

---
+++ index.js
@@ -0,0 +1,7 @@
+// export * from "./lib/client/oberknecht.client";
+// export * from "./lib/export.mjs";
+
+require("./lib/parser/privmsg.Message");
+require("./lib/parser/globaluserstate.Message");
+
+require("./lib/client/oberknecht.client");
