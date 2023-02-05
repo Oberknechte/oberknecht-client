5f1c1af99295b1ec3a4c29ead21c1a922e2a6f11
jubewe <ju.be.we@posteo.de>
2023-02-05 23:37:44 +0100

    vb-0.0.5

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // export * from "./lib/client/oberknecht.client";
 // export * from "./lib/export.mjs";
 
+const oberknechtClient = require("./lib/client/oberknecht.client");
+
 // require("./lib/emitters/oberknecht.emitter.callbacks");
 
 require("./lib/client/oberknecht.client");
 require("./lib/parser/Message.Types");
+
+// module.exports = {
+//     oberknechtClient: oberknechtClient,
+//     ...require("./lib/parser/Message.Types"),
+// };
