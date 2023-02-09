affe09c6c4a78e8ee629b0ad743f12718a16a2ab
jubewe <ju.be.we@posteo.de>
2023-02-09 14:20:13 +0100

    vb-0.0.7-4

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+require("./lib/parser/CLEARCHAT.Message");
+require("./lib/parser/GLOBALUSERSTATE.Message");
+require("./lib/parser/NOTICE.Message");
+require("./lib/parser/PRIVMSG.Message");
+require("./lib/parser/USERNOTICE.Message");
+require("./lib/parser/USERSTATE.Message");
+require("./lib/parser/WHISPER.Message");
+
+require("./lib/emitters/oberknecht.emitter");
+require("./lib/emitters/oberknecht.action.emitter");
+// This is all because JS won't show any values on the callbacks unless I import them here  FeelsWeirdMan
+
 module.exports = class {
     static oberknechtClient = require("./lib/client/oberknecht.client");
     static messageTypes = require("./lib/parser/Message.Types");
