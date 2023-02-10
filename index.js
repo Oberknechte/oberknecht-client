d044e7638009ad60b1aa1ab66e9544184ce400df
jubewe <ju.be.we@posteo.de>
2023-02-10 14:52:29 +0100

    vb-0.0.8

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ require("./lib/parser/WHISPER.Message");
 
 require("./lib/emitters/oberknecht.emitter");
 require("./lib/emitters/oberknecht.action.emitter");
+
+require("./lib/operations/action");
+require("./lib/operations/join");
+require("./lib/operations/joinAll");
+require("./lib/operations/part");
+require("./lib/operations/partAll");
+require("./lib/operations/ping");
+require("./lib/operations/pong");
+require("./lib/operations/privmsg");
+require("./lib/operations/reply");
+require("./lib/operations/sendraw");
+require("./lib/operations/whisper");
 // This is all because JS won't show any values on the callbacks unless I import them here  FeelsWeirdMan
 
 module.exports = class {
