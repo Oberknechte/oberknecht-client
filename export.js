b9a55f57ab3cfb5cd96cb36448c4743f71241fde
jubewe <ju.be.we@posteo.de>
2023-06-06 13:20:17 +0200

    v1.0.2

--- export.js
+++ export.js
@@ -1,1 +1,6 @@
+const messageTypes = require("./lib-js/parser/Message.Types");
+const clientOptions = require("./lib-js/types/oberknechtClient.clientOptions");
+
 module.exports = require("./lib-js/client/oberknecht.client");
+module.exports = messageTypes;
+module.exports = clientOptions;
--- export.ts
+++ export.ts
@@ -1,1 +1,3 @@
 export * from "./lib-ts/client/oberknecht.client";
+export * as messageTypes from "./lib-ts/parser/Message.Types";
+export { clientOptions } from "./lib-js/types/oberknechtClient.clientOptions";
