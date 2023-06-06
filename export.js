3a7d9f7e440c96f59d754332dce351c7a2896200
jubewe <ju.be.we@posteo.de>
2023-06-06 13:27:24 +0200

    v1.0.4

--- export.js
+++ export.js
@@ -1,6 +1,7 @@
+const { oberknechtClient } = require("./lib-js/client/oberknecht.client");
 const { messageTypes } = require("./lib-js/parser/Message.Types");
 const { clientOptions } = require("./lib-js/types/oberknechtClient.clientOptions");
 
-exports.oberknechtClient = require("./lib-js/client/oberknecht.client");
+exports.oberknechtClient = oberknechtClient;
 exports.messageTypes = messageTypes;
 exports.clientOptions = clientOptions;
